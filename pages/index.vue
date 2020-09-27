ed9f2c128ba2ae05a6117e3e8ac9d49794bdcd96
Nelson Bonilla <nelson.bonilla@synapse.com>
2020-09-27 17:20:22 -0300

    conectado con woocomerce

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,46 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        emes
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <h1>{{prueba}}</h1>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
-export default Vue.extend({})
+  const WooCommerce = new WooCommerceRestApi({
+      url: 'http://localhost/emes',
+      consumerKey: 'ck_325eca4a49fb6d5f7cc5926c30fe24b902e505d9',
+      consumerSecret: 'cs_425299ebef1291e4c4ea09f5b261abfa99a2447d',
+      version: 'wc/v3',
+      // queryStringAuth: true // Force Basic Authentication as query string true and using under HTTPS
+  })
+
+
+  const App = Vue.extend({
+    data() {
+      return {
+        prueba: 'Hola Mundo'
+      } 
+    },
+    methods: {
+      init() {
+        WooCommerce.get("products/categories")
+        .then((response: any) => {
+            console.log(response.data);
+            // return res.status(200).json(response.data);
+        })
+        .catch((error: any) => {
+            console.log(error.response.data);
+            // return res.status(400).json(error.response.data);
+        });
+      }
+    },
+    created() {
+      this.init();
+    }
+    
+  })
+
+export default App;
 </script>
 
 <style>
