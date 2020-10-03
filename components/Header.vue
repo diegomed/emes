66adf4c227e10f5128caec79d380d47d168dad4a
Nelson Bonilla <nelson.bonilla@synapse.com>
2020-10-03 18:39:29 -0300

    Componentes listos

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">EMES s.r.l</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <b-nav-item href="#">Cetegorías</b-nav-item>
+            <b-nav-item href="#">Conócenos</b-nav-item>
+            <b-nav-item href="#">Contacto</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Buscar</b-button>
+            </b-nav-form>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,85 +1,61 @@
 <template>
-  <h1>{{prueba}}</h1>
+  <div>
+    <Carousel/>
+    <b-container>
+      <b-row>
+        <h3 class="pa3 gray">Productos destacados</h3>
+      </b-row>
+      <b-row>
+        <b-col>
+          <Product/>
+        </b-col>
+        <b-col>
+          <Product/>
+        </b-col>
+        <b-col>
+          <Product/>
+        </b-col>
+        <b-col>
+          <Product/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+  import Vue from 'vue';
+  // import WooCommerce from 'middleware/woocommerce';
 
-  const WooCommerce = new WooCommerceRestApi({
-      url: 'http://localhost/emes',
-      consumerKey: 'ck_325eca4a49fb6d5f7cc5926c30fe24b902e505d9',
-      consumerSecret: 'cs_425299ebef1291e4c4ea09f5b261abfa99a2447d',
-      version: 'wc/v3',
-      // queryStringAuth: true // Force Basic Authentication as query string true and using under HTTPS
-  })
-
-
-  const App = Vue.extend({
-    data() {
-      return {
-        prueba: 'Hola Mundo'
-      } 
-    },
-    methods: {
-      init() {
-        WooCommerce.get("products/categories")
-        .then((response: any) => {
-            console.log(response.data);
-            // return res.status(200).json(response.data);
-        })
-        .catch((error: any) => {
-            console.log(error.response.data);
-            // return res.status(400).json(error.response.data);
-        });
-      }
-    },
-    created() {
-      this.init();
-    }
     
-  })
 
-export default App;
+    const App = Vue.extend({
+      data() {
+        return {
+          prueba: 'Hola Mundo'
+        } 
+      },
+      methods: {
+        init(): void {
+          // WooCommerce.get("products/categories")
+          // .then((response: any) => {
+          //     console.log(response.data);
+          //     // return res.status(200).json(response.data);
+          // })
+          // .catch((error: any) => {
+          //     console.log(error.response.data);
+          //     // return res.status(400).json(error.response.data);
+          // });
+        }
+      },
+      created() {
+        this.init();
+      }
+    })
+
+  export default App;
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
