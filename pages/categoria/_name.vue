b883a7b87f86e053865258c405e6e426cb28ae26
Nelson Bonilla <nelson.bonilla@synapse.com>
2020-10-11 17:01:51 -0300

    avanzando

---
+++ pages/categoria/_name.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="bg-primary py-3">
+      <h3 class="p-3 text-white text-center font-weight-normal">Categoría: <b>Bebidas</b></h3>
+    </div>
+    <b-container class="mt-5">
+      <b-row>
+        <b-col  cols="12" md="4" lg="3">
+          <div>
+            <h5>Categorías</h5>
+            <ul>
+              <li>Link</li>
+            </ul>
+          </div>
+        </b-col>
+        <b-col cols="12" md="8" lg="9">
+          <b-row>
+            <b-col cols="6" md="4" lg="3" >
+              <Product/>
+            </b-col>
+            <b-col cols="6" md="4" lg="3" >
+              <Product/>
+            </b-col>
+            <b-col cols="6" md="4" lg="3" >
+              <Product/>
+            </b-col>
+            <b-col cols="6" md="4" lg="3" >
+              <Product/>
+            </b-col>
+            <b-col cols="6" md="4" lg="3" >
+              <Product/>
+            </b-col>
+          </b-row>
+        </b-col>
+
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  // import WooCommerce from 'middleware/woocommerce';
+
+    
+
+    const Categories = Vue.extend({
+      data() {
+        return {
+          prueba: 'Hola Mundo'
+        } 
+      },
+      methods: {
+        init(): void {
+          // WooCommerce.get("products/categories")
+          // .then((response: any) => {
+          //     console.log(response.data);
+          //     // return res.status(200).json(response.data);
+          // })
+          // .catch((error: any) => {
+          //     console.log(error.response.data);
+          //     // return res.status(400).json(error.response.data);
+          // });
+        }
+      },
+      created() {
+        this.init();
+      }
+    })
+
+  export default Categories;
+</script>
+
+<style>
+
+</style>
---
+++ pages/producto/_slug.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      Esto es un producto
+  </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+
+    const WooCommerce = new WooCommerceRestApi({
+        url: 'http://localhost/emes',
+        consumerKey: 'ck_325eca4a49fb6d5f7cc5926c30fe24b902e505d9',
+        consumerSecret: 'cs_425299ebef1291e4c4ea09f5b261abfa99a2447d',
+        version: 'wc/v3',
+        // queryStringAuth: true // Force Basic Authentication as query string true and using under HTTPS
+    })
+    
+
+    const ProductView = Vue.extend({
+      data() {
+        return {
+          prueba: 'Hola Mundo'
+        } 
+      },
+      methods: {
+        init(): void {
+          WooCommerce.get("products?slug=vino-santa-teresa")
+          .then((response: any) => {
+              console.log(response.data);
+              // return res.status(200).json(response.data);
+          })
+          .catch((error: any) => {
+              console.log(error.response.data);
+              // return res.status(400).json(error.response.data);
+          });
+        }
+      },
+      created() {
+        this.init();
+      }
+    })
+
+  export default ProductView;
+</script>
+
+<style>
+
+</style>
