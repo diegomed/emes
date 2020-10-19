4389283d5133f32f8b27a39bd82083ec56a6eb1a
Nelson Bonilla <nelson.bonilla@synapse.com>
2020-10-18 22:56:32 -0300

    paginado listo

---
+++ components/Pagination.vue
@@ -0,0 +1,73 @@
+<template>
+    <nav class="py-5" aria-label="..." v-if="totalPages > 0">
+        <ul class="pagination justify-content-center">
+            <li class="page-item" v-bind:class="{disabled: next().currentPage === 1}">
+                <a class="page-link" :aria-disabled="prev().currentPage === 1" :href="prev().path+'?'+prev().query">Anterior</a>
+            </li>
+            <li class="page-item" v-for="page in createPages(totalPages)" :key="page.number" v-bind:class="{active: isActive(page.currentPage, page.number)}">
+                <span v-if="page.currentPage == page.number" class="page-link">
+                    {{page.number}}
+                    <span class="sr-only">(current)</span>
+                </span>
+                <a v-else class="page-link"  :href="page.path+'?'+page.query">{{page.number}}</a>
+            </li>
+            <li class="page-item" v-bind:class="{disabled: prev().currentPage === totalPages}">
+                <a class="page-link" :aria-disabled="prev().currentPage === totalPages" :href="next().path+'?'+next().query">Siguiente</a>
+            </li>
+        </ul>
+    </nav>
+
+</template>
+
+<script lang="ts">
+    import Vue, { PropOptions } from 'vue';
+    import { Product } from '~/interfaces/interfaces';
+    export default Vue.extend ({
+        props: {
+            totalPages: {
+                type: Number,
+                required: true
+            } as PropOptions<number>,
+            path: {
+                type: String,
+                required: true
+            } as PropOptions<string>,
+            query: {
+                type: Object,
+                required: true
+            } as PropOptions<string>,
+        },
+        methods: {
+            prev(): string {
+                return this.createObjectPage(this.query.page && parseFloat(this.query.page) > 1 ? parseFloat(this.query.page) -1 : 1);
+            },
+            next(): string {
+                return this.createObjectPage(this.query.page && parseFloat(this.query.page) < this.totalPages ? parseFloat(this.query.page) + 1 : this.totalPages);
+            },
+            isActive(index, current): boolean {
+                return parseFloat(index) === parseFloat(current)
+            },
+            createPages(pages: number): any {
+                const pagesObject = [];
+                for (let index = 1; index <= this.totalPages; index++) { 
+                    pagesObject.push(this.createObjectPage(index)) 
+                }
+                return pagesObject;
+            },
+            createObjectPage(index: number, ): any {
+                return {
+                    number: index,
+                    query: Object.keys(this.query).filter(key => key !== 'page').map(key => (
+                        key + '=' + this.query[key]
+                    )).join('&').concat(Object.keys(this.query).length > 0 ? `&page=${index}`: `page=${index}`),
+                    path: this.path,
+                    currentPage: this.query.page ? parseFloat(this.query.page) : 1
+                }
+            }
+        }
+    })
+</script>
+
+<style>
+
+</style>
--- pages/categoria/_name.vue
+++ pages/categoria/_name.vue
@@ -5,7 +5,7 @@
     </div>
     <b-container class="mt-5">
       <b-row>
-        <b-col  cols="12" md="4" lg="3">
+        <b-col cols="12" md="4" lg="3">
           <div>
             <h5>Categorías</h5>
             <CategoriesProductList :categories="categoryList"/>
@@ -18,6 +18,11 @@
             </b-col>
             <b-col  v-if="!products.length">
               <h4 class="text-secondary text-center p-4">No se encontraron productos en la categoría {{category.name}}</h4>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <Pagination :totalPages="totalPages" :path="path" :query="query" />
             </b-col>
           </b-row>
         </b-col>
@@ -41,26 +46,56 @@
         ]
       }
     },
-    async asyncData({params, $api}) {
+    async asyncData({params, $api, query, route}) {
       const categoryData: {data: Category[]} = await $api.get(`products/categories?slug=${params.name}`);
-      const productList: {data: Product[]} = await $api.get(`products?category=${categoryData.data[0].id}`);
+      const productList: {headers: any, data: Product[]} = await $api.get(`products?category=${categoryData.data[0].id}&page=${query.page ? query.page : 1}&per_page=1`);
       const categoryList: {data: Category[]} = await $api.get(`products/categories`);
+      // console.log(productList.headers)
       return {
               category: categoryData.data[0],
               products: productList.data,
-              categoryList: categoryList.data
+              categoryList: categoryList.data,
+              totalPages: parseFloat(productList.headers['x-wp-totalpages']),
+              query,
+              path: route.path
             }
     },
     data() {
       return {
         category: {}  as Category,
         products: [],
-        categoryList: [] as Category[]
+        categoryList: [] as Category[],
+        totalPages: 0 as number,
+        query: {},
+        params: {}
       } 
     },
     methods: {
       init(): void {
-      }
+      },
+      // isActive(index, current): boolean {
+      //   console.log(index, current)
+      //   return parseFloat(index) === parseFloat(current)
+      // },
+      // createPages(pages: number): any {
+      //   const pagesObject = [];
+      //   for (let index = 1; index <= 10; index++) { 
+      //     pagesObject.push({
+      //       number: index,
+      //       query: Object.keys(this.query).map(key => {
+      //         if(key === 'page') {
+      //           return `page=${index}`
+      //         } else {
+      //           return key + '=' + this.query[key]
+      //         }
+              
+      //       }).join('&'),
+      //       path: this.path,
+      //       currentPage: this.query.page ? this.query.page : 1
+      //     }) 
+      //   }
+      //   return pagesObject;
+      // }
     },
     created() {
       this.init();
